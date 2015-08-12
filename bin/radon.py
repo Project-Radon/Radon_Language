e29c668645a226e07c3277a4e4892e7de363fe03
Minerguy67880 <theminerguy67880@gmail.com>
2015-08-11 19:39:42 -0500

    Started on integer parsing and added an error if there is no input file. Write is also case insensitive now.

--- bin/radon.py
+++ bin/radon.py
@@ -3,13 +3,18 @@ from sys import *
 tokens = []
 
 def open_file(filename):
-    data = open(filename, "r").read()
-    return data
+    if filename == None:
+        print("There is no input file.")
+    elif filename != None:
+        data = open(filename, "r").read()
+        return data
 
 def lex(filecontents):
         tok = ""
         state = 0
         string = ""
+        expr = ""
+        nm = ""
         filecontents = list(filecontents)
         for char in filecontents:
                 tok += char
@@ -18,11 +23,24 @@ def lex(filecontents):
                         tok = ""
                     else:
                         tok = " "
-                elif tok == "\n":
+                elif tok == "\n" or tok == "<EOF>":
+                    if expr != "" and isexpr == 1:
+                        print(expr + "Int")
+                        expr = ""
+                    elif expr != "" and isexpr == 0:
+                        print(expr + "Int")
+                        expr = ""
                     tok = ""
-                elif tok == "write":
+                elif tok.lower() == "write":
                         tokens.append("write")
                         tok = ""
+                elif tok == "0" or tok == "1" or tok == "2" or tok == "3" or tok == "4" or tok == "5" or tok == "6" or tok == "7" or tok == "8" or tok == "9":
+                    expr += tok
+                    tok = ""
+                elif tok == "+":
+                    isexpr = 1
+                    expr += tok
+                    tok = ""
                 elif tok == "\"":
                         if state == 0:
                                 state = 1
@@ -34,20 +52,24 @@ def lex(filecontents):
                 elif state == 1:
                         string += tok
                         tok = ""
-                print(tokens)
+
+                print(expr)
         return tokens
 def parse(toks):
     v = 0
     print(v)
     while(v < len(toks)):
         if toks[v] + " " + toks[v+1][0:6] == "write string":
-            print("Found string")
+            print(toks[v+1][7:])
             v += 2
     
 def run():
-    data = open_file(argv[1])
-    toks = lex(data)
-    parse(toks)
+    if len(argv) > 1:
+        data = open_file(argv[1])
+        toks = lex(data)
+        parse(toks)
+    elif len(argv) < 2:
+        print("There is no input file.")
 
 def main():
     run()
